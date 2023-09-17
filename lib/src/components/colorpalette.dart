3c5f20ef76ab5be439bc2d66d5fb54380b58620a
abdooamr <abdooamr25@gmail.com>
2023-09-17 06:13:03 +0300

    Create colorpalette.dart

---
+++ lib/src/components/colorpalette.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+List<Color> purpleGradient = [
+  Color.fromARGB(255, 38, 18, 79), // Dark purple
+  Color.fromARGB(255, 63, 49, 112), // Purple transitioning to blue
+  Color.fromARGB(255, 89, 86, 173), // Blue transitioning to teal
+  Color.fromARGB(255, 56, 173, 169) // Modern teal
+];
+List<Color> redGradient = [
+  Color.fromARGB(255, 82, 10, 28), // Dark red
+  Color.fromARGB(255, 173, 20, 87), // Dark red transitioning to vibrant red
+  Color.fromARGB(255, 255, 87, 34) // Modern vibrant red
+];
+
+List<Color> greenGradient = [
+  Color.fromARGB(255, 18, 63, 20), // Dark green
+  Color.fromARGB(255, 25, 111, 61), // Dark green transitioning to vibrant green
+  Color.fromARGB(255, 76, 187, 23) // Modern vibrant green
+];
+List<Color> blackGradient = [
+  Color.fromARGB(255, 15, 15, 15), // Very dark gray
+  Color.fromARGB(255, 30, 30, 30), // Darker gray
+  Color.fromARGB(255, 60, 60, 60), // Dark gray
+  Color.fromARGB(255, 90, 90, 90), // Medium gray
+  Color.fromARGB(255, 120, 120, 120), // Light gray
+  Color.fromARGB(255, 150, 150, 150) // Lighter gray
+];
+List<Color> pinkGradient = [
+  Color.fromARGB(255, 223, 180, 195), // Soft pastel pink
+  Color.fromARGB(
+      255, 255, 111, 207), // Pastel pink transitioning to vibrant pink
+  Color.fromARGB(255, 255, 0, 132) // Modern vibrant pink
+];
+List<Color> blueGradient = [
+  Color.fromARGB(255, 23, 24, 35), // Darkest purple
+  Color.fromARGB(255, 19, 20, 29), // Dark purple
+  Color.fromARGB(255, 33, 35, 51), // Darker purple
+  Color.fromARGB(255, 54, 51, 90), // Purple transitioning to blue
+  Color.fromARGB(255, 87, 79, 132), // Deep purple transitioning to blue
+  Color.fromARGB(255, 140, 120, 171), // Deep blue
+  Color.fromARGB(255, 87, 143, 207), // Medium blue
+  Color.fromARGB(255, 122, 170, 220), // Blue transitioning to light blue
+  Color.fromARGB(255, 173, 216, 230) // Light blue
+];
